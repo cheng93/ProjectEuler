0c96eacb903ef4cc825a3c509bd57ac41d703ef7
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 11:02:30 +0000

    Problem 0005

---
+++ Solutions/0005/Program.cs
@@ -0,0 +1,20 @@
+﻿long Solution(int limit)
+    => Enumerable.Range(1, limit)
+        .Aggregate(1L, (agg, cur) => Lcm(agg, cur));
+
+long Lcm(long a, long b)
+    => (a * b) / Gcd(a, b);
+
+long Gcd(long a, long b)
+{
+    while (b != 0)
+    {
+        var t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+Console.WriteLine(Solution(10)); // 2520
+Console.WriteLine(Solution(20));
