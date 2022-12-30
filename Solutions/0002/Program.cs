4866d8ae1f6de33795d27db3e89bfe516afb4162
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 08:40:08 +0000

    Problem 0002

---
+++ Solutions/0002/Program.cs
@@ -0,0 +1,23 @@
+﻿int Solution(int limit)
+    => Fibonacci()
+        .TakeWhile(x => x <= limit)
+        .Where(x => x % 2 == 0)
+        .Sum();
+
+IEnumerable<int> Fibonacci()
+{
+    var a = 1;
+    var b = 2;
+    yield return a;
+    yield return b;
+    while (true)
+    {
+        var sum = a + b;
+        yield return sum;
+        a = b;
+        b = sum;
+    }
+}
+
+Console.WriteLine(Solution(100)); // 44
+Console.WriteLine(Solution(4000000));
