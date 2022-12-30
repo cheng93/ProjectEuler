9ab37e924dd5824f8677f815f4c0e22545ea3f8f
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 12:43:27 +0000

    Problem 0009

---
+++ Solutions/0009/Program.cs
@@ -0,0 +1,19 @@
+﻿int Solution(int sum)
+{
+    for (var i = 1; i < sum / 3; i++)
+    {
+        for (var j = i + 1; j <= (sum - i - 1) / 2; j++)
+        {
+            var k = sum - i - j;
+            if (i * i + j * j == k * k)
+            {
+                return i * j * k;
+            }
+        }
+    }
+
+    throw new Exception();
+}
+
+Console.WriteLine(Solution(12)); // 60
+Console.WriteLine(Solution(1000));
