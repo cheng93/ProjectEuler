2c0f521d6ccf84d1bd7996be9b4724deecf3a17a
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 13:32:27 +0000

    Problem 0010

---
+++ Solutions/0010/Program.cs
@@ -0,0 +1,21 @@
+﻿long Solution(int limit) => Sieve(limit).Sum();
+
+IEnumerable<long> Sieve(int n)
+{
+    var nonPrimes = new HashSet<long>();
+    for (var i = 2L; i <= n; i++)
+    {
+        if (nonPrimes.Contains(i))
+        {
+            continue;
+        }
+        yield return i;
+        for (var j = i * i; j <= n; j += i)
+        {
+            nonPrimes.Add(j);
+        }
+    }
+}
+
+Console.WriteLine(Solution(10)); // 17
+Console.WriteLine(Solution(2000000));
