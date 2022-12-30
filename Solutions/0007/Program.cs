ecddf0561fb1215e6d79aaace2c5396a96bcac29
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 11:49:26 +0000

    Problem 0007

---
+++ Solutions/0007/Program.cs
@@ -0,0 +1,58 @@
+﻿int Solution(int limit)
+    => Primes().Take(limit).Last();
+
+IEnumerable<int> Primes()
+{
+    yield return 2;
+    var i = 3;
+    while (true)
+    {
+        if (!Factor(i).Any())
+        {
+            yield return i;
+        }
+        i++;
+    }
+}
+
+IEnumerable<int> Factor(long n)
+{
+    var quotient = n;
+    var sqrt = (int)Math.Ceiling(Math.Sqrt(n));
+    foreach (var prime in Sieve(sqrt))
+    {
+        if (quotient % prime == 0)
+        {
+            yield return prime;
+            while (quotient % prime == 0)
+            {
+                quotient /= prime;
+            }
+        }
+        if (quotient == 1)
+        {
+            yield break;
+        }
+    }
+}
+
+IEnumerable<int> Sieve(int n)
+{
+    var nonPrimes = new HashSet<int>();
+    for (var i = 2; i <= n; i++)
+    {
+        if (nonPrimes.Contains(i))
+        {
+            continue;
+        }
+        yield return i;
+        for (var j = i * i; j <= n; j += i)
+        {
+            nonPrimes.Add(j);
+        }
+    }
+}
+
+
+Console.WriteLine(Solution(6)); // 13
+Console.WriteLine(Solution(10001));
