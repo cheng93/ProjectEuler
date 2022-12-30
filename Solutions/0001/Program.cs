af08922c2db18295bada80d2f45ea2f409f45921
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 08:27:08 +0000

    Problem 0001

---
+++ Solutions/0001/Program.cs
@@ -0,0 +1,8 @@
+﻿int Solution(int limit)
+    => Enumerable
+        .Range(0, limit)
+        .Where(x => x % 3 == 0 || x % 5 == 0)
+        .Sum();
+
+Console.WriteLine(Solution(10)); // 23
+Console.WriteLine(Solution(1000));
