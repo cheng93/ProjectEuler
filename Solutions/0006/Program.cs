a4740852f22f9f8a0c525c4a10948b11f46f294e
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 11:27:15 +0000

    Problem 0006

---
+++ Solutions/0006/Program.cs
@@ -0,0 +1,15 @@
+﻿int Solution(int limit)
+{
+    var sum = Sum(limit);
+    var sumOfSquares = SumOfSquares(limit);
+
+    return sum * sum - sumOfSquares;
+}
+
+int Sum(int limit) => (limit * (limit + 1)) / 2;
+
+int SumOfSquares(int limit) => (limit * (limit + 1) * (2 * limit + 1)) / 6;
+
+
+Console.WriteLine(Solution(10)); // 2640
+Console.WriteLine(Solution(100));
