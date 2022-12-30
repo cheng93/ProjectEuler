f7b08a29e05ecb94e049ded7c8fbd046299dffa1
cheng93 <derek.c@hotmail.co.uk>
2022-12-30 16:14:03 +0000

    Problem 0012

---
+++ Solutions/0012/Program.cs
@@ -0,0 +1,32 @@
+﻿int Solution(int length) => Triangle().First(x => Factors(x).Count() > length);
+
+IEnumerable<int> Triangle()
+{
+    var sum = 0;
+    var i = 1;
+    while (true)
+    {
+        sum += i;
+        yield return sum;
+        i++;
+    }
+}
+
+IEnumerable<int> Factors(int n)
+{
+    var sqrt = (int)Math.Sqrt(n);
+    for (var i = 1; i <= sqrt; i++)
+    {
+        if (n % i == 0)
+        {
+            yield return i;
+            if (i != n / i)
+            {
+                yield return n / i;
+            }
+        }
+    }
+}
+
+Console.WriteLine(Solution(5)); // 28
+Console.WriteLine(Solution(500));
