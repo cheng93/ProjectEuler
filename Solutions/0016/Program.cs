db4db40311d8b64dfe9fe3592361b96e18625f9b
cheng93 <derek.c@hotmail.co.uk>
2022-12-31 23:53:11 +0000

    Problem 0016

---
+++ Solutions/0016/Program.cs
@@ -0,0 +1,39 @@
+﻿int Solution(int power)
+{
+    var product = "1";
+    for (var i = 0; i < power; i++)
+    {
+        product = Square(product);
+    }
+
+
+    return product
+        .Select(x => x.ToString())
+        .Select(int.Parse)
+        .Sum();
+}
+
+string Square(string n)
+{
+    var carry = 0;
+    var product = string.Empty;
+
+    for (var i = 1; i <= n.Length; i++)
+    {
+        var unit = int.Parse(n[^i].ToString());
+        var multiplied = unit * 2;
+        multiplied += carry;
+        carry = multiplied / 10;
+        product = multiplied % 10 + product;
+    }
+
+    if (carry > 0)
+    {
+        product = carry + product;
+    }
+
+    return product;
+}
+
+Console.WriteLine(Solution(15)); // 26
+Console.WriteLine(Solution(1000));
