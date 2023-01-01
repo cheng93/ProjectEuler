c065e5f9705c4e58b47a164056993422fedee649
cheng93 <derek.c@hotmail.co.uk>
2023-01-01 12:17:04 +0000

    Problem 0022

---
+++ Solutions/0022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+int Solution() => NameScores().Sum();
+
+IEnumerable<int> NameScores()
+{
+    var path = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!, "names.txt");
+    var str = File.ReadAllText(path);
+    var names = str.Split(",")
+        .Select(x => x[1..(^1)])
+        .OrderBy(x => x)
+        .ToArray();
+
+    for (var i = 0; i < names.Length; i++)
+    {
+        yield return (i + 1) * AlphabeticalValue(names[i]);
+    }
+}
+
+int AlphabeticalValue(string name)
+{
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return name.Select(x => alphabet.IndexOf(x) + 1).Sum();
+}
+
+Console.WriteLine(Solution());
