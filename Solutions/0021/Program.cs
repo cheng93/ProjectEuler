166188e2790bbe127b746fed984625352740ad51
cheng93 <derek.c@hotmail.co.uk>
2023-01-01 11:57:13 +0000

    Problem 0021

---
+++ Solutions/0021/Program.cs
@@ -0,0 +1,49 @@
+﻿int Solution(int limit) => Amicable().TakeWhile(x => x < limit).Sum();
+
+IEnumerable<int> Amicable()
+{
+    var found = new HashSet<int>();
+
+    var i = 1;
+    while (true)
+    {
+        if (found.Contains(i))
+        {
+            yield return i;
+        }
+        else
+        {
+            var sum = SumOfProperDivisors(i);
+            if (sum > i && SumOfProperDivisors(sum) == i)
+            {
+                yield return i;
+                found.Add(sum);
+            }
+        }
+        i++;
+    }
+}
+
+int SumOfProperDivisors(int n)
+{
+    var properDivisors = Factors(n).Where(x => x != n);
+    return properDivisors.Sum();
+}
+
+IEnumerable<int> Factors(int n)
+{
+    var sqrt = (int)Math.Sqrt(n);
+    for (var i = 1; i <= sqrt; i++)
+    {
+        if (n % i == 0)
+        {
+            yield return i;
+            if (i != n / i)
+            {
+                yield return n / i;
+            }
+        }
+    }
+}
+
+Console.WriteLine(Solution(10000));
