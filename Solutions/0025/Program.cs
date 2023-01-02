5bf645850b559fda1a86ad9cc62c80fb60f628fd
cheng93 <derek.c@hotmail.co.uk>
2023-01-02 07:58:52 +0000

    Problem 0025

---
+++ Solutions/0025/Program.cs
@@ -0,0 +1,49 @@
+﻿int Solution(int length)
+    => Fibonacci()
+        .Select((x, i) => (N: x, Index: i + 1))
+        .First(x => x.N.Length == length)
+        .Index;
+
+IEnumerable<string> Fibonacci()
+{
+    var a = "1";
+    var b = "1";
+    yield return a;
+    yield return b;
+    while (true)
+    {
+        var t = b;
+        b = Sum(a, b);
+        yield return b;
+        a = t;
+    }
+}
+
+string Sum(string a, string b)
+{
+    if (a == "0") return b;
+    if (b == "0") return a;
+
+    var head = a.Length >= b.Length
+        ? a[..(a.Length - b.Length)]
+        : b[..(b.Length - a.Length)];
+
+    var sum = string.Empty;
+    var carry = 0;
+    for (var i = 1; i <= Math.Min(a.Length, b.Length); i++)
+    {
+        var added = int.Parse(a[^i].ToString()) + int.Parse(b[^i].ToString()) + carry;
+        sum = added % 10 + sum;
+        carry = added / 10;
+    }
+
+    if (carry > 0)
+    {
+        head = Sum(head, carry.ToString());
+    }
+
+    return head + sum;
+}
+
+Console.WriteLine(Solution(3)); // 12
+Console.WriteLine(Solution(1000));
---
+++ Solutions/0025/README.md
@@ -0,0 +1,24 @@
+# 1000-digit Fibonacci number
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+$$F_n = F_{n−1} + F_{n−2}, where F_1 = 1 and F_2 = 1$$
+
+Hence the first 12 terms will be:
+
+$F_1 = 1$  
+$F_2 = 2$  
+$F_3 = 3$  
+$F_4 = 5$  
+$F_5 = 8$  
+$F_6 = 13$  
+$F_7 = 21$  
+$F_8 = 34$  
+$F_9 = 45$  
+$F_{10} = 55$  
+$F_{11} = 89$  
+$F_{12} = 144$
+
+The 12th term, $F_{12}$, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
