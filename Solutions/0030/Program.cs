db08ec9d80746a8cdaff43483cef4fd2d787689b
cheng93 <derek.c@hotmail.co.uk>
2023-01-03 08:14:47 +0000

    Problem 0030

---
+++ Solutions/0030/Program.cs
@@ -0,0 +1,48 @@
+﻿int Solution(int power) => SumOfPowerDigits(power).Sum();
+
+IEnumerable<int> SumOfPowerDigits(int power)
+{
+    var units = Enumerable
+        .Range(0, 10)
+        .Select(x => Enumerable
+            .Range(x, power)
+            .Aggregate((agg, _) => agg * x))
+        .ToArray();
+
+    var max = Max();
+
+    for (var i = 10; i < max; i++)
+    {
+        var quotient = i;
+        var sum = 0;
+        while (quotient != 0)
+        {
+            var remainder = quotient % 10;
+            sum += units[remainder];
+            quotient /= 10;
+        }
+
+        if (sum == i)
+        {
+            yield return i;
+        }
+    }
+
+    int Max()
+    {
+        var number = 9;
+        var sum = units[9];
+
+        while (sum > number)
+        {
+            sum += units[9];
+            number *= 10;
+            number += 9;
+        }
+
+        return number;
+    }
+}
+
+Console.WriteLine(Solution(4)); // 19316
+Console.WriteLine(Solution(5));
