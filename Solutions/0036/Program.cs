0b1bf7e7be5f04e0bf95ecea96949faddc18a780
cheng93 <derek.c@hotmail.co.uk>
2023-01-04 21:30:36 +0000

    Problem 0036

---
+++ Solutions/0036/Program.cs
@@ -0,0 +1,29 @@
+﻿int Solution(int limit)
+    => DoubleBasePalindromes()
+        .TakeWhile(x => x < limit)
+        .Sum();
+
+IEnumerable<int> DoubleBasePalindromes()
+{
+    var i = 1;
+    while (true)
+    {
+        if (IsPalindrome(i.ToString()))
+        {
+            var binary = Convert.ToString(i, 2);
+            if (IsPalindrome(binary))
+            {
+                yield return i;
+            }
+        }
+        i += 2;
+    }
+}
+
+bool IsPalindrome(string str)
+{
+    var half = str.Length / 2;
+    return str[..half] == string.Join(string.Empty, str[(^half)..].Reverse());
+}
+
+Console.WriteLine(Solution(1000000));
