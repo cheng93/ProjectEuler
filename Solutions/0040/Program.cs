a5cfcc2cb5bc6a9f837647c39274ad1e5981f081
cheng93 <derek.c@hotmail.co.uk>
2023-01-05 19:44:06 +0000

    Problem 0040

---
+++ Solutions/0040/Program.cs
@@ -0,0 +1,40 @@
+﻿int Solution()
+{
+    var i = 1;
+    var n = 1;
+    var product = 1;
+
+    foreach (var digit in ChampernowneDigits())
+    {
+        if (n == i)
+        {
+            product *= digit;
+            n *= 10;
+            if (i == 1000000)
+            {
+                break;
+            }
+        }
+        i++;
+    }
+
+    return product;
+}
+
+IEnumerable<int> ChampernowneDigits()
+{
+    var n = 1;
+    while (true)
+    {
+        var exponent = (int)Math.Log10(n);
+        for (var i = exponent; i >= 0; i--)
+        {
+            var pow = (int)Math.Pow(10, i);
+            var digit = (n % (pow * 10)) / pow;
+            yield return digit;
+        }
+        n++;
+    }
+}
+
+Console.WriteLine(Solution());
---
+++ Solutions/0040/README.md
@@ -0,0 +1,11 @@
+# Champernowne's constant
+
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.12345678910**1**112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If $d_n$ represents the nth digit of the fractional part, find the value of the following expression.
+
+$d_1 × d_{10} × d_{100} × d_{1000} × d_{10000} × d_{100000} × d_{1000000}$
