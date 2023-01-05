f7ffb6a5f9351dc570b17ff518ce93090b549d4d
cheng93 <derek.c@hotmail.co.uk>
2023-01-05 08:59:33 +0000

    Problem 0039

---
+++ Solutions/0039/Program.cs
@@ -0,0 +1,24 @@
+﻿int Solution()
+    => IntegerRightTriangles()
+    .GroupBy(x => x)
+    .OrderByDescending(x => x.Count())
+    .First().Key;
+
+IEnumerable<int> IntegerRightTriangles()
+{
+    for (var i = 1; i <= 332; i++)
+    {
+        var pMax = (999 - i) / 2;
+        for (var j = i + 1; j <= pMax; j++)
+        {
+            var h = Math.Sqrt(i * i + j * j);
+            var perimeter = i + j + (int)h;
+            if (h % 1 == 0 && perimeter <= 1000)
+            {
+                yield return i + j + (int)h;
+            }
+        }
+    }
+}
+
+Console.WriteLine(Solution());
