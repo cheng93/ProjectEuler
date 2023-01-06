2a28a818cbea79f034242bb040fee4bc84d8cdba
cheng93 <derek.c@hotmail.co.uk>
2023-01-06 21:23:15 +0000

    Problem 0047

---
+++ Solutions/0047/Program.cs
@@ -0,0 +1,59 @@
+﻿int Solution(int n)
+{
+    var i = 1;
+    while (true)
+    {
+        for (var j = 0; j < n; j++)
+        {
+            if (PrimeFactors(i + j).Count() >= n)
+            {
+                if (j == n - 1)
+                {
+                    return i;
+                }
+            }
+            else
+            {
+                i += j + 1;
+                break;
+            }
+        }
+    }
+}
+
+IEnumerable<int> PrimeFactors(int n)
+{
+    var seen = new HashSet<int>();
+    while (n % 2 == 0)
+    {
+        if (!seen.Contains(2))
+        {
+            yield return 2;
+            seen.Add(2);
+        }
+        n /= 2;
+    }
+
+
+    for (var i = 3; i < Math.Sqrt(n); i += 2)
+    {
+        while (n % i == 0)
+        {
+            if (!seen.Contains(i))
+            {
+                yield return i;
+                seen.Add(i);
+            }
+            n /= i;
+        }
+    }
+
+    if (n > 2)
+    {
+        yield return n;
+    }
+}
+
+Console.WriteLine(Solution(2)); // 14
+Console.WriteLine(Solution(3)); // 644
+Console.WriteLine(Solution(4));
