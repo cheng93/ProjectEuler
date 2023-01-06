eef93f9185af7fce937fe7e94ef3d138d5b58dc9
cheng93 <derek.c@hotmail.co.uk>
2023-01-06 20:24:37 +0000

    Problem 0045

---
+++ Solutions/0045/Program.cs
@@ -0,0 +1,23 @@
+﻿long Solution() => Tph().SkipWhile(x => x <= 40755).First();
+
+IEnumerable<long> Tph()
+{
+    var i = 1;
+    while (true)
+    {
+        var triangle = Triangle(i);
+        if (IsPentagonal(triangle))
+        {
+            Console.WriteLine(triangle);
+            yield return triangle;
+        }
+        i += 2;
+    }
+}
+
+long Triangle(long n) => (n * (n + 1)) / 2;
+
+bool IsPentagonal(long n)
+    => Math.Sqrt(24 * n + 1) % 6 == 5;
+
+Console.WriteLine(Solution());
